78470998606ecf06e74e083f4b4f47c298526a76
Andrzej Pronobis <a.pronobis@gmail.com>
2014-12-17 14:54:30 -0800

    Added license.

--- generate_theme.py
+++ generate_theme.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
+# material-linux - Auto-generated set of consistent themes for a Linux
+#                  desktop based on the Android Material color palette.
+#
+# Copyright (C) 2014 Andrzej Pronobis
+# Author: Andrzej Pronobis <a.pronobis@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import re
 from x256 import x256
